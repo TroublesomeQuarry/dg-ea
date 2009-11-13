b9ab4079b4c3e5e7ba0f616d7b1a3ee3c65d08da
darraghgrogan <darraghgrogan@gmail.com>
2009-11-13 16:02:45 +0000

    pc

---
+++ EAServer/src/wb/EcjRequest.java
@@ -0,0 +1,38 @@
+package wb;
+
+public class EcjRequest {
+
+	private String verb;
+	private String boby;
+	private String jobName;
+	
+	public String getJobName() {
+		return jobName;
+	}
+
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+
+	public String getVerb() {
+		return verb;
+	}
+
+	public void setVerb(String verb) {
+		this.verb = verb;
+	}
+
+	public String getBoby() {
+		return boby;
+	}
+
+	public void setBoby(String boby) {
+		this.boby = boby;
+	}
+
+	public EcjRequest(String messageText) {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
